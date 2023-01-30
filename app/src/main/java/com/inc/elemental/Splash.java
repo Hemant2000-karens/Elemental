555e3f2413c0cc553a926085798b6dbe74a3ffdf
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2023-01-30 20:13:58 +0530

    Completed Splash.java

--- app/src/main/java/com/inc/elemental/Splash.java
+++ app/src/main/java/com/inc/elemental/Splash.java
@@ -2,13 +2,24 @@ package com.inc.elemental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
+    private int timer = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }, timer);
     }
 }

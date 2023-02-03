85c0833b57a37e998832f2bcf9d83e21784fbf10
Hemant Kumar <team.karensbook@icloud.com>
2023-02-03 22:49:01 +0530

    Completed Login Page

--- app/src/main/java/com/inc/elemental/MainActivity.java
+++ app/src/main/java/com/inc/elemental/MainActivity.java
@@ -1,8 +1,10 @@
 package com.inc.elemental;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.text.Html.fromHtml;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.textView3)).setText(fromHtml("Facotry<sup>TM</sup>",0));
     }
 }

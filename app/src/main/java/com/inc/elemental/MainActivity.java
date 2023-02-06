ca8ea287db107b63f6c9481c3bddd571e5040812
Hemant Kumar <team.karensbook@icloud.com>
2023-02-06 11:11:01 +0530

    improved OTP Page

--- app/src/main/java/com/inc/elemental/MainActivity.java
+++ app/src/main/java/com/inc/elemental/MainActivity.java
@@ -3,7 +3,11 @@ package com.inc.elemental;
 import static android.text.Html.fromHtml;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((TextView)findViewById(R.id.textView3)).setText(fromHtml("Facotry<sup>TM</sup>",0));
+        ((TextView)findViewById(R.id.textView3)).setText(fromHtml("Factory<sup>TM</sup>",0));
+
+        Button btn = findViewById(R.id.verybtn);
+        btn.setOnClickListener(view -> {
+            //Write Code Here
+        });
+
     }
 }

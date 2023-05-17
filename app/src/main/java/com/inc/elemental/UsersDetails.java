b2858b360dc47f3f7c537d3d9230886348fa4308
Hemant Kumar <team.karensbook@icloud.com>
2023-05-17 19:40:41 +0530

    update

--- app/src/main/java/com/inc/elemental/UsersDetails.java
+++ app/src/main/java/com/inc/elemental/UsersDetails.java
@@ -3,6 +3,7 @@ package com.inc.elemental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class UsersDetails extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class UsersDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_details);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.genderSpinner);
+
+
+
+
+
+
+
     }
 }
